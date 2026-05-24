8dc4b15f9b6b4f33c566c74753f9373f1edef918
Alexey Dyryvolkov <ald@siscon.dk>
2026-05-25 00:15:00 +0200

    Исправил свой комментарий в models Создал метод для создания Topic и Post-ответа

--- forum/services.py
+++ forum/services.py
@@ -1,7 +1,11 @@
+from django.utils import timezone
 from core.models import User
-from forum.models import Category, Post
+from .models import Category, Post, Topic
 from django.db import transaction
 from django.db.models import F
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.core.files.uploadedfile import UploadedFile
 
 class CategoryServise:
     @staticmethod
@@ -23,6 +27,44 @@ class CategoryServise:
         )
     
 
+class TopicService:
+    @classmethod
+    @transaction.atomic
+    def create_topic_with_post(
+        cls,
+        category:Category,
+        author:User,
+        title:str,
+        content:str,
+        image: UploadedFile | None = None
+    ) -> Topic:
+        """
+        Атомарно создает новую тему и корневое сообщение для нее.
+        """
+
+        if not title.strip():
+            raise ValidationError("Заголовок темы не может быть пустым.")
+        if not content.strip():
+            raise ValidationError("Текст первого сообщения не может быть пустым.")
+        
+        topic = Topic.objects.create(
+            category=category,
+            author=author,
+            title=title.strip(),
+        )
+
+        Post.objects.create(
+            topic=topic,
+            author=author,
+            content=content.strip(),
+            parent=None,
+            image=image,
+        )
+
+        return topic
+
+    
+
 class PostService:
     @staticmethod
     def toggle_like(post: Post, user: User) -> bool:
@@ -39,3 +81,38 @@ class PostService:
             post.liked_by.add(user)
             Post.objects.filter(id=post.pk).update(likes_count=F('likes_count')+1)
             return True
+        
+
+    @classmethod
+    @transaction.atomic
+    def create_reply(
+        cls,
+        topic: Topic,
+        author: User,
+        content: str,
+        parent: Post,
+        image: UploadedFile|None = None
+    ) -> Post:
+        """
+        Создает новый пост-ответ в теме и атомарно обновляет метрики темы.
+        """
+
+        if not content.strip():
+            raise ValidationError("Сообщение не может быть пустым.")
+        if topic.is_closed:
+            raise ValidationError("Нельзя писать в закрытую тему.")
+        
+        post = Post.objects.create(
+            topic=topic,
+            author=author,
+            content=content.strip(),
+            parent=parent,
+            image=image
+        )
+
+        Topic.objects.filter(pk=topic.pk).update(
+            replies_count=F('replies_count') + 1,
+            last_active=timezone.now()
+        )
+
+        return post
