c9def5cf33407bec5e8a540c5c0691efac37c8b7
Olga <xotaoshi@gmail.com>
2026-05-24 22:40:42 +0300

    temp files

--- project/static/js/index.js
+++ project/static/js/index.js
@@ -1,20 +1,31 @@
 document.addEventListener('DOMContentLoaded', ()=> {
     
-    var elems = document.querySelectorAll('select');
-    var options = document.querySelectorAll('option');
-    var instances = M.FormSelect.init(elems, options);
+    // var elems = document.querySelectorAll('select');
+    // var options = document.querySelectorAll('option');
+    // var instances = M.FormSelect.init(elems, options);
 
     
-    const myButton = document.getElementById('myButton');
-    if (myButton) {
-        myButton.addEventListener('click', ()=> {
-            alert('Clicked!');
-        });
-    }
+    // const myButton = document.getElementById('myButton');
+    // if (myButton) {
+    //     myButton.addEventListener('click', ()=> {
+    //         alert('Clicked!');
+    //     });
+    // }
     
 
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+    });
+
+    // Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+    // var collapsibleElem = document.querySelector('.collapsible');
+    // var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+    // Or with jQuery
+    $(document).ready(function(){
+    $('.sidenav').sidenav();
+    });
 
     
-});
+
 
 console.log("Hello from index.js!");
