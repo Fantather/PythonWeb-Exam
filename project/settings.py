3b0aba9b0c961ff45e64b94e35560892c728c663
Olga <xotaoshi@gmail.com>
2026-05-26 17:45:52 +0300

    modified:   core/admin.py 	new file:   core/forms.py 	modified:   core/migrations/0001_initial.py 	modified:   core/models.py 	modified:   db.sqlite3 	modified:   forum/forms.py 	modified:   forum/migrations/0001_initial.py 	new file:   media/icons/backgroung.svg 	new file:   media/icons/pngwing.com.png 	new file:   media/icons/pngwing_4BWsNUW.com.png 	new file:   media/icons/pngwing_8kdPpD9.com.png 	new file:   media/icons/pngwing_zt1NmgG.com.png 	modified:   project/settings.py 	modified:   requirements.txt

--- project/settings.py
+++ project/settings.py
@@ -42,6 +42,7 @@ AUTH_USER_MODEL = 'core.User'
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -132,3 +133,40 @@ STATICFILES_DIRS = [BASE_DIR / 'project' / 'static']
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / "media"
+
+
+JAZZMIN_SETTINGS = {
+    "show_ui_builder": False,
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": True,
+    "footer_small_text": True,
+    "body_small_text": False,
+    "brand_small_text": True,
+    "brand_colour": False,
+    "accent": "accent-primary",
+    "navbar": "navbar-white navbar-light",
+    "no_navbar_border": True,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": True,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-dark-primary",
+    "sidebar_nav_small_text": True,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": False,
+    "theme": "flatly",
+    "default_theme_mode": "dark",
+    "button_classes": {
+        "primary": "btn-outline-primary",
+        "secondary": "btn-outline-secondary",
+        "info": "btn-outline-info",
+        "warning": "btn-outline-warning",
+        "danger": "btn-outline-danger",
+        "success": "btn-outline-success"
+    }
+}
